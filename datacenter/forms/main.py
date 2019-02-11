6c14e472004b96d8ba1210c534f9aa7592514ce5
yuquant <569721942@qq.com>
2019-02-11 20:00:51 +0800

    基本完工

--- datacenter/forms/main.py
+++ datacenter/forms/main.py
@@ -35,26 +35,24 @@ class CommentForm(FlaskForm):
 
 
 class ResearchForm(FlaskForm):
-    title = StringField('1.研究题目', validators=[DataRequired(), Length(1, 50)],
+    title = StringField('研究题目*', validators=[DataRequired(), Length(1, 50)],
                         render_kw={'placeholder': '必填', 'class': 'add-on'},
                         )
-    time_wait = FloatField('6.传输间隔（分钟）', default=5, validators=[NumberRange(0, 20)])
-
-    # researcher = StringField('2.研究者（*必填）', validators=[DataRequired(), Length(1, 20)])
-    transport_id = SelectField('4.DICOM图像传输', coerce=int, choices='', validators=[InputRequired()])
-    folder_name = StringField('&nbsp&nbsp下载后的文件夹命名',
-                              validators=[Length(0, 50)],
-                              render_kw={'placeholder': '不修改则按照默认规则命名', 'title': '不修改则按照默认规则命名'},)
-    series = StringField('5.序列描述',
-                         render_kw={'placeholder': '不填代表全部序列,谨慎填写', 'title': '不填代表全部序列,谨慎填写'},)
-    other_file = FileField('8.其他相关文件')
-    patients = TextAreaField('3.检查号列表', validators=[DataRequired()],
+    patients = TextAreaField('检查号列表*', validators=[DataRequired()],
                              render_kw={'placeholder': '必填,换行符隔开', 'title': '必填,换行符隔开'},)
 
-    research_plan = CKEditorField('7.研究说明',
-                                  render_kw={'placeholder': '可复制doc文档到此处', 'title': '可复制doc文档到此处'},)
+    transport_id = SelectField('DICOM图像传输*', coerce=int, choices='', validators=[InputRequired()])
 
-    # anonymous = BooleanField('是否匿名为Nifty', default=0)
+    folder_name = StringField('下载后的文件夹命名',
+                              validators=[Length(0, 50)],
+                              render_kw={'placeholder': '不修改则按照默认规则命名', 'title': '不修改则按照默认规则命名'},)
+    time_wait = FloatField('传输间隔（分钟）', default=5, validators=[NumberRange(0, 20)])
+    series = StringField('序列描述',
+                         render_kw={'placeholder': '不填代表全部序列;谨慎填写,填写时用英文逗号隔开可能的序列描述',
+                                    'title': '不填代表全部序列;谨慎填写,填写时用英文逗号隔开可能的序列描述'},)
+    other_file = FileField('其他相关文件')
+    research_plan = CKEditorField('研究说明',
+                                  render_kw={'placeholder': '可复制doc文档到此处', 'title': '可复制doc文档到此处'},)
     submit = SubmitField('提交')
 
     def __init__(self, *args, **kwargs):
