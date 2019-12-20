622a2db0cc361bec41f84e624c090b6f408179b7
yuquant <569721942@qq.com>
2019-12-20 11:22:35 +0800

    log

--- dcmtks/back_process.py
+++ dcmtks/back_process.py
@@ -6,11 +6,29 @@ Description :
 """
 from time import sleep
 import os
+import logging
+import traceback
 from sqlalchemy import and_
 from configparser import ConfigParser
 from datacenter import db, create_app
 from datacenter.models import Tasks, AEDict, Patients
 from dcmtks.pydcmtk import DcmTrans
+LOGGER = logging.getLogger(__name__)
+
+
+class PatientStatus:
+    SUCCESS = 1
+    CANCEL = 2
+    FAIL = 3
+
+
+class TaskStatus:
+    SUCCESS = 1
+    CANCEL = 2
+    FAIL = 3
+    PARTLY_FINISHED = 4
+    ERROR = 5
+    WAITING = 7
 
 
 def back_server():
@@ -28,7 +46,7 @@ def back_server():
     while True:
         # 队列中任务
         flag = 1  # 未置顶,task没有变化
-        task = Tasks.query.filter(Tasks.status_id == 7).order_by(Tasks.priority.desc()).order_by(
+        task = Tasks.query.filter(Tasks.status_id == TaskStatus.WAITING).order_by(Tasks.priority.desc()).order_by(
             Tasks.timestamp).first()
         # 查询任务状态为待处理的优先级最高,同一优先级按时间排序
         if task:
@@ -43,34 +61,39 @@ def back_server():
                     series_desc = task.series
                 output_dir = make_output_dir_for_dicom(task)
                 # output_dir = os.path.join('downloads', task.researcher.username, task.folder_name, 'images')
-                print(output_dir)
+                LOGGER.info(output_dir)
                 dt = DcmTrans(server_ip=server_ip, server_port=server_port, aec=aec, aet=aet,
                               my_port=client_port, output_dir=output_dir)
                 for i, patient in enumerate(patients):
                     # 实时查询当前任务是否被取消
                     # ratio = str((i+1) / len(patients) * 100)
                     # 如果任务状态为待处理则继续进行
-                    if task.status_id == 7:  # 队列中
+                    if task.status_id == TaskStatus.WAITING:  # 队列中
                         accession_no = patient.accession_no
                         try:
-                            print(accession_no, transport_to)
+                            LOGGER.info(accession_no+transport_to)
                             if transport_to == 'DOWNLOAD':
                                 dt.download_dcms(AccessionNumber=accession_no, SeriesDescription=series_desc)
+                                if os.listdir(os.path.join(output_dir, accession_no)):
+                                    patient.status_id = PatientStatus.SUCCESS  # 完成
+                                else:
+                                    patient.err_message = '离线数据'
+                                    patient.status_id = PatientStatus.FAIL  # 失败
                             else:
                                 dt.move(AccessionNumber=accession_no, aem=transport_to, SeriesDescription=series_desc)
-                            patient.status_id = 1  # 完成
+                                patient.status_id = PatientStatus.SUCCESS  # 完成
                         except Exception as e:
-                            print(e)
+                            LOGGER.info(traceback.format_exc())
                             patient.err_message = str(e)[:70]
-                            patient.status_id = 3  # 失败
+                            patient.status_id = PatientStatus.FAIL  # 失败
                         db.session.commit()
                         if i != len(patients) - 1:
                             sleep(task.time_wait * 60)
-                    elif task.status_id == 2:  # 任务被取消
-                        patient.status_id = 2  # 取消
+                    elif task.status_id == TaskStatus.CANCEL:  # 任务被取消
+                        patient.status_id = PatientStatus.CANCEL  # 取消
 
                     # 判断任务是否被切换,切换则跳出for循环
-                    new_task = Tasks.query.filter(Tasks.status_id == 7).order_by(Tasks.priority.desc()).order_by(
+                    new_task = Tasks.query.filter(Tasks.status_id == TaskStatus.WAITING).order_by(Tasks.priority.desc()).order_by(
                         Tasks.timestamp).first()
                     if (not new_task) or new_task.id != task.id:
                         task.active = False
@@ -78,29 +101,28 @@ def back_server():
                         flag = 0
                         break
 
-                if flag and task.status_id == 7:
+                if flag and task.status_id == TaskStatus.WAITING:
                     err_count = Patients.query.filter(
-                        and_(Patients.task_id == task.id, Patients.status_id == 3)).count()
+                        and_(Patients.task_id == task.id, Patients.status_id == PatientStatus.FAIL)).count()
                     count = Patients.query.filter(Patients.task_id == task.id).count()
                     failed_percent = err_count / count * 100
                     if failed_percent == 100:
                         # print('任务失败')
-                        task.status_id = 3  # 任务失败
+                        task.status_id = TaskStatus.FAIL  # 任务失败
                     elif failed_percent > 0:
-                        task.status_id = 4  # 部分完成
+                        task.status_id = TaskStatus.PARTLY_FINISHED  # 部分完成
                     elif failed_percent == 0:
-                        task.status_id = 1  # 完成
+                        task.status_id = TaskStatus.SUCCESS  # 完成
                     else:
                         raise(Warning, 'Unexpected failed_percent:{}'.format(failed_percent))
 
             except Exception as e:
-                print('未知错误', e)
-                task.status_id = 5  # 未知错误
+                LOGGER.info(traceback.format_exc())
+                task.status_id = TaskStatus.ERROR  # 未知错误
             task.active = False
             db.session.commit()
 
         sleep(10)
-
 
 
 def make_output_dir_for_dicom(task):
--- tests/logtest.py
+++ tests/logtest.py
@@ -4,9 +4,7 @@ Author : Jason
 Github : https://github.com/yuquant
 Description : 
 """
-from threading import Thread
-from dcmtks.back_process import back_server
-from datacenter import create_app
+
 import os
 import logging
 from logging.handlers import RotatingFileHandler
@@ -48,14 +46,7 @@ def set_logger():
     # logging.getLogger('').addHandler(rotateHandler)
 
 
-app = create_app(config_name='development')
-
 if __name__ == "__main__":
     set_logger()
     logger = get_logger(__name__, log_path=None, debug=True)
     logger.info('datacenter starting')
-    app.config['back_task'] = Thread(target=back_server)
-    app.config['back_task'].setDaemon(True)
-    app.config['back_task'].start()
-    # debug=False才能保证后台程序正常运行
-    app.run(host='0.0.0.0', port=8000, debug=False, threaded=True)
